438d28fe894dca35fab84168d296dcf91854c49a
Moraru Ilinca <ilincaanamoraru@gmail.com>
2025-03-21 21:19:49 +0200

    changed sides encoding to acomodate for more than 4 sides and irregular sides

--- fragment.py
+++ fragment.py
@@ -1,4 +1,19 @@
+import cv2 as cv
+import numpy as np 
+
 class Fragment:
     def __init__(self,value, fragment_idx):
         self.value = value
         self.fragment_idx = fragment_idx
+
+        # to do: side detection
+        # for now, hardcoded
+#--------------------------------------------------------------------
+        h, w = self.value.shape[:2]
+        self.corners = [[0,0],[0,h-1], [w-1,h-1], [w-1,0]]
+#---------------------------------------------------------------------
+        binary_mask =  (self.value[:, :, 3])
+        contours, _ = cv.findContours(binary_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+        contour = max(contours, key=cv.contourArea)  
+        contour = contour.reshape(-1, 2)
+        self.contour = contour
--- side.py
+++ side.py
@@ -1,6 +1,7 @@
 class Side:
-    def __init__(self, value, side_type, fragment_idx):
+    def __init__(self, value, side_indexes_of_fragment, side_idx, fragment_idx):
         self.value = value.astype(int)
-        self.side_type = side_type
+        self.side_indexes_of_fragment = side_indexes_of_fragment
+        self.side_idx = side_idx
         self.fragment_idx = fragment_idx
 
